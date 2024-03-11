eea8788dc0f83ef694d00828dbdfbb5217a24691
Bashir23Pro <95079580+Bashir23G@users.noreply.github.com>
2024-03-11 13:29:39 +0100

    added sound effect

--- StudioGame/Assets/Scripts/Gun.cs
+++ StudioGame/Assets/Scripts/Gun.cs
@@ -6,8 +6,23 @@ public class Gun : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
-    public Rigidbody Aircraft;
+   
     public float bulletSpeed = 0.3f;
+
+
+    // Gun Sound effect
+
+    public AudioClip soundClip; // Der Sound-Clip, den du abspielen möchtest
+    private AudioSource audioSource; // Der AudioSource-Komponente
+
+
+      void Start()
+    {
+        // Füge eine AudioSource-Komponente zum GameObject hinzu
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = soundClip;
+        audioSource.Stop(); // Stelle sicher, dass der Sound zu Beginn gestoppt ist
+    }
     
     void Update()
     {
@@ -15,12 +30,32 @@ public class Gun : MonoBehaviour
        
         if (Input.GetKeyDown(KeyCode.O))
         {
+
            
             var bullet = Instantiate(bulletPrefab,bulletSpawnPoint.position,bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity=8*(bulletSpawnPoint.forward*bulletSpeed);
+           // aktiv sound
+
+            PlaySound();
             
         }
+
+          
+       
     }
 
+void PlaySound()
+    {
+        // Überprüfe, ob die AudioSource-Komponente und der Sound-Clip vorhanden sind
+        if (audioSource != null && soundClip != null)
+        {
+            // Spiele den Sound ab
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("AudioSource oder Sound-Clip fehlt.");
+        }
+    }
    
 }
